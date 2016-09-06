e0ca35c72f65272471d68a69d57981933b773d2a
Cody Sehl <csehl@pivotal.io>
2016-09-06 17:50:55 -0600

    Added dynamic framework for Carthage consumption
    Signed-off-by: Sasha Heinen <sheinen@pivotal.io>

---
+++ WYPopoverController/WYPopoverController/WYPopoverController.h
@@ -0,0 +1,20 @@
+//
+//  WYPopoverController.h
+//  WYPopoverController
+//
+//  Created by pivotal on 9/6/16.
+//  Copyright © 2016 pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WYPopoverController.
+FOUNDATION_EXPORT double WYPopoverControllerVersionNumber;
+
+//! Project version string for WYPopoverController.
+FOUNDATION_EXPORT const unsigned char WYPopoverControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WYPopoverController/PublicHeader.h>
+
+#import <WYPopoverController/WYPopoverController.h>
+#import <WYPopoverController/WYStoryboardPopoverSegue.h>
